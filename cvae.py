d51d5dbb72ac3e377bd4e2bbe2496ca7a1f5d414
wilfredkisku <wilfred.kisku@gmail.com>
2021-03-14 00:35:47 +0530

    changes cvae

--- cvae.py
+++ cvae.py
@@ -48,3 +48,38 @@ class CVAE(tf.keras.Model):
                     tf.keras.layers.Dense(latent_dim + latent_dim),
                 ]
                 )
+        self.decoder = tf.keras.Sequential(
+                [
+                    tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+                    tf.keras.layers.Dense(units=7*7*32, activation='relu'),
+                    tf.keras.layers.Reshape(target_shape=(7,7,32)),
+                    tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same', activation='relu'),
+                    tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same', activation='relu'),
+                    tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=3, strides=1, padding='same'),
+                ]
+                )
+    
+    #randomly sample epsilon for reparameteize 
+    @tf.function
+    def sample(self, eps=None):
+        if eps is None:
+            eps = tf.random.normal(shape=(100, self.latent_dim))
+        return self.decode(eps, apply_sigmoid=True)
+
+    #encode pipeline
+    def encode(self, x):
+        mean, logvar = tf.split(self.encode(x), num_of_size_splits=2, axis=1)
+        return mean, logvar
+    
+    #reparameterize 
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.eps(logvar * 0.5) + mean
+    
+    #decode pipline
+    def decode(self, z, apply_sigmoid=False):
+        logits = self.decode(z)
+        if apply_sigmoid:
+            probs = tf.sigmoid(logits)
+            return probs
+        return logits
