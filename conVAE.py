3efd5bc6b8f905c97e048e23cdadb1534601024d
wilfredkisku <wilfred.kisku@gmail.com>
2021-03-15 14:28:46 +0530

    changes in all

--- conVAE.py
+++ conVAE.py
@@ -83,3 +83,37 @@ class CVAE(tf.keras.Model):
             probs = tf.sigmoid(logits)
             return probs
         return logits
+
+optimizer = tf.keras.optimizers.Adam(1e-4)
+
+def log_normal_pdf(sample, mean, logvar, raxis=1):
+    log2pi = tf.math.log(2.*np.pi)
+    return tf.reduce_sum(-.5 * ((sample-mean)**2 * tf.exp(-logvar) + logvar + log2pi), axis=raxis)
+
+def compute_loss(model, x):
+    #obtain the mean and logvar through the encoder
+    mean, logvar = model.encode(x)
+    z = model.reparameterize(mean, logvar)
+
+    #obtain the logits in the higher dimension through decoder
+    x_logit = model.decoder(z)
+    
+    #obtain the cross entropy loss
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+
+    #obtain the logpx_z, logpz and logpz_x
+    logpx_z = -tf.reduce_sum(cross_ent, axis=[1,2,3])
+    logpz = log_normal_pdf(z, 0., 0.)
+    logpz_x = log_normal_pdf(z, mean, logvar)
+    return -tf.reduce_mean(logpx_z + logpz - logpz_x)
+
+@tf.function
+def train_step(model, x, optimizer):
+
+    with tf.GradientTape() as tape:
+        loss = compute_loss(model, x)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+
+
